bce472baa4d9263380e85545ef60cd4f2b004eb4
Isaiah19 <nwezeifeanyi93@gmail.com>
2024-01-19 14:00:49 -0800

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
-#Install flask / Version must be 2.1.0
-package { 'flask':
+#Install flask version 2.1.0
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.0.1', # Use a compatible version with Flask 2.1.0
+  provider => 'pip3',
 }
